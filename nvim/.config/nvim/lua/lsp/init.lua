85f25b15e82bbb54433b96367887173ca0edca81
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2024-02-05 17:06:41 +0100

    chore(neovim): removed old configs unusued files

--- nvim/.config/nvim/lua/lsp/init.lua
+++
@@ -1,8 +0,0 @@
--- Secure calling lspconfig plugin
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
--- requiring the file that is in charge of configuring lsp
-require("lsp.lsp-installer")
-require("lsp.handlers").setup()
