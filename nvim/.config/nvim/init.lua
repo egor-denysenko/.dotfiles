99e3219e745479fda20e49c52085ba6c4c4449d2
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2024-04-24 13:33:41 +0200

    chore(nvim): added several testing tools into current configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -47,7 +47,8 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim',
+      {
+        'williamboman/mason.nvim',
         config = true,
         ensure_installed = {
           "gopls",
@@ -81,6 +82,16 @@ require('lazy').setup({
   },
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
+
+  -- DAP support and language specific stuff
+  { 'mfussenegger/nvim-dap' },
+  -- GO debugging stuff
+  {
+    'leoluz/nvim-dap-go',
+    ft = "go",
+  },
+  { 'rcarriga/nvim-dap-ui',  requires = { 'mfussenegger/nvim-dap' } },
+
   -- lazy.nvim
   -- Extra configuration layer for neovim UI
   {
@@ -96,7 +107,7 @@ require('lazy').setup({
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
-      }
+    }
   },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
@@ -237,10 +248,10 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-    {
-     "folke/trouble.nvim",
-     dependencies = { "nvim-tree/nvim-web-devicons" },
-     opts = {
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
       -- TODO move this config to a separate file in order to reduce clutter
 
       -- your configuration comes here
@@ -265,7 +276,7 @@ require('lazy').setup({
     -- Language specific plugins
     {
       "ray-x/go.nvim",
-      dependencies = {  -- optional packages
+      dependencies = { -- optional packages
         "ray-x/guihua.lua",
         "neovim/nvim-lspconfig",
         "nvim-treesitter/nvim-treesitter",
@@ -273,12 +284,34 @@ require('lazy').setup({
       config = function()
         require("go").setup()
       end,
-      event = {"CmdlineEnter"},
-      ft = {"go", 'gomod'},
+      event = { "CmdlineEnter" },
+      ft = { "go", 'gomod' },
       build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
     },
-  }
-
+  },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        -- config
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+  },
+  {
+      "NStefan002/speedtyper.nvim",
+      cmd = "Speedtyper",
+      opts = {
+      -- your config
+      }
+  },
+  {
+      "polirritmico/monokai-nightasty.nvim",
+      lazy = false,
+      priority = 1000,
+  },
+  { "nvim-neotest/nvim-nio" },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -296,7 +329,9 @@ require('lazy').setup({
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
--- NOTE: You can change these options as you wish!
+
+vim.opt.background = "dark" -- default to dark or light style
+vim.cmd.colorscheme("monokai-nightasty")
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -415,7 +450,7 @@ local function live_grep_git_root()
 end
 
 -- Trouble keybinds
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, {desc = 'Toggle trouble'})
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, { desc = 'Toggle trouble' })
 vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
 vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
 vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
@@ -593,13 +628,13 @@ require('which-key').register({
 -- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
-require('go').setup{
+require('go').setup {
   lsp_cfg = false
   -- other setups...
 }
-local cfg = require'go.lsp'.config() -- config() return the go.nvim gopls setup
+local gopls_cfg = require 'go.lsp'.config() -- config() return the go.nvim gopls setup
 
-require('lspconfig').gopls.setup(cfg)
+require('lspconfig').gopls.setup(gopls_cfg)
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -611,7 +646,7 @@ require('lspconfig').gopls.setup(cfg)
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
@@ -619,7 +654,7 @@ local servers = {
 
   lua_ls = {
     Lua = {
-      workspace = { checkThirdParty = false },
+      workspace = { checkThir },
       telemetry = { enable = false },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
@@ -631,26 +666,205 @@ local servers = {
 require('neodev').setup()
 
 require('go').setup()
-require("go.format").gofmt()  -- gofmt only
-require("go.format").goimport()  -- goimport + gofmt
+
+-- Go DAP adapter
+require('dap-go').setup {
+  -- Additional dap configurations can be added.
+  -- dap_configurations accepts a list of tables where each entry
+  -- represents a dap configuration. For more details do:
+  -- :help dap-configuration
+  dap_configurations = {
+    {
+      -- Must be "go" or it will be ignored by the plugin
+      type = "go",
+      name = "Attach remote",
+      mode = "remote",
+      request = "attach",
+    },
+  },
+  -- delve configurations
+  delve = {
+    -- the path to the executable dlv which will be used for debugging.
+    -- by default, this is the "dlv" executable on your PATH.
+    path = "dlv",
+    -- time to wait for delve to initialize the debug session.
+    -- default to 20 seconds
+    initialize_timeout_sec = 20,
+    -- a string that defines the port to start delve debugger.
+    -- default to string "${port}" which instructs nvim-dap
+    -- to start the process in a random available port
+    port = "${port}",
+    -- additional args to pass to dlv
+    args = {},
+    -- the build flags that are passed to delve.
+    -- defaults to empty string, but can be used to provide flags
+    -- such as "-tags=unit" to make sure the test suite is
+    -- compiled during debugging, for example.
+    -- passing build flags using args is ineffective, as those are
+    -- ignored by delve in dap mode.
+    build_flags = "",
+  },
+}
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp',
+  function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
 
 -- Run gofmt + goimport on save
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').goimport()
+   require('go.format').goimports()
   end,
   group = format_sync_grp,
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('go').setup({
-  -- other setups ....
-  lsp_cfg = {
-    capabilities = capabilities,
-    -- other setups
+  disable_defaults = true, -- true|false when true set false to all boolean settings and replace all table
+  -- settings with {}
+  go = 'go', -- go command, can be go[default] or go1.18beta1
+  fillstruct = 'gopls', -- default, can also use fillstruct
+  gofmt = 'gofumpt', --gofmt cmd,
+  max_line_len = 128, -- max line length in golines format, Target maximum line length for golines
+  tag_transform = false, -- can be transform option("snakecase", "camelcase", etc) check gomodifytags for details and more options
+  tag_options = 'json=omitempty', -- sets options sent to gomodifytags, i.e., json=omitempty
+  gotests_template = "", -- sets gotests -template parameter (check gotests for details)
+  gotests_template_dir = "", -- sets gotests -template_dir parameter (check gotests for details)
+  comment_placeholder = '', -- comment_placeholder your cool placeholder e.g. 󰟓       
+  icons = { breakpoint = '🧘', currentpos = '🏃' }, -- setup to `false` to disable icons setup
+  verbose = false, -- output loginf in messages
+  lsp_cfg = false, -- true: use non-default gopls setup specified in go/lsp.lua
+  -- false: do nothing
+  -- if lsp_cfg is a table, merge table with with non-default gopls setup in go/lsp.lua, e.g.
+  --   lsp_cfg = {settings={gopls={matcher='CaseInsensitive', ['local'] = 'your_local_module_path', gofumpt = true }}}
+  lsp_gofumpt = false, -- true: set default gofmt in gopls format to gofumpt
+  lsp_on_attach = nil, -- nil: use on_attach function defined in go/lsp.lua,
+  --      when lsp_cfg is true
+  -- if lsp_on_attach is a function: use this function as on_attach function for gopls
+  lsp_keymaps = true,  -- set to false to disable gopls/lsp keymap
+  lsp_codelens = true, -- set to false to disable codelens, true by default, you can use a function
+  -- function(bufnr)
+  --    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>F", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap=true, silent=true})
+  -- end
+  -- to setup a table of codelens
+  diagnostic = {  -- set diagnostic to false to disable vim.diagnostic setup
+    hdlr = false, -- hook lsp diag handler and send diag to quickfix
+    underline = true,
+    -- virtual text setup
+    virtual_text = { spacing = 0, prefix = '■' },
+    signs = true,
+    update_in_insert = false,
   },
+  lsp_document_formatting = true,
+  -- set to true: use gopls to format
+  -- false if you want to use other formatter tool(e.g. efm, nulls)
+  lsp_inlay_hints = {
+    enable = true,
+    -- hint style, set to 'eol' for end-of-line hints, 'inlay' for inline hints
+    -- inlay only avalible for 0.10.x
+    style = 'inlay',
+    -- Note: following setup only works for style = 'eol', you do not need to set it for 'inlay'
+    -- Only show inlay hints for the current line
+    only_current_line = false,
+    -- Event which triggers a refersh of the inlay hints.
+    -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+    -- not that this may cause higher CPU usage.
+    -- This option is only respected when only_current_line and
+    -- autoSetHints both are true.
+    only_current_line_autocmd = "CursorHold",
+    -- whether to show variable name before type hints with the inlay hints or not
+    -- default: false
+    show_variable_name = true,
+    -- prefix for parameter hints
+    parameter_hints_prefix = "󰊕 ",
+    show_parameter_hints = true,
+    -- prefix for all the other hints (type, chaining)
+    other_hints_prefix = "=> ",
+    -- whether to align to the lenght of the longest line in the file
+    max_len_align = false,
+    -- padding from the left if max_len_align is true
+    max_len_align_padding = 1,
+    -- whether to align to the extreme right or not
+    right_align = false,
+    -- padding from the right if right_align is true
+    right_align_padding = 6,
+    -- The color of the hints
+    highlight = "Comment",
+  },
+  gopls_cmd = nil,          -- if you need to specify gopls path and cmd, e.g {"/home/user/lsp/gopls", "-logfile","/var/log/gopls.log" }
+  gopls_remote_auto = true, -- add -remote=auto to gopls
+  gocoverage_sign = "█",
+  sign_priority = 5,        -- change to a higher number to override other signs
+  dap_debug = true,         -- set to false to disable dap
+  dap_debug_keymap = true,  -- true: use keymap for debugger defined in go/dap.lua
+  -- false: do not use keymap in go/dap.lua.  you must define your own.
+  -- Windows: Use Visual Studio keymap
+  dap_debug_gui = {},                                            -- bool|table put your dap-ui setup here set to false to disable
+  dap_debug_vt = { enabled_commands = true, all_frames = true }, -- bool|table put your dap-virtual-text setup here set to false to disable
+
+  dap_port = 38697,                                              -- can be set to a number, if set to -1 go.nvim will pick up a random port
+  dap_timeout = 15,                                              --  see dap option initialize_timeout_sec = 15,
+  dap_retries = 20,                                              -- see dap option max_retries
+  build_tags = "tag1,tag2",                                      -- set default build tags
+  textobjects = true,                                            -- enable default text objects through treesittter-text-objects
+  test_runner = 'go',                                            -- one of {`go`, `richgo`, `dlv`, `ginkgo`, `gotestsum`}
+  verbose_tests = true,                                          -- set to add verbose flag to tests deprecated, see '-v' option
+  run_in_floaterm = false,                                       -- set to true to run in a float window. :GoTermClose closes the floatterm
+  -- float term recommend if you use richgo/ginkgo with terminal color
+  --
+  floaterm = {             -- position
+    posititon = 'auto',    -- one of {`top`, `bottom`, `left`, `right`, `center`, `auto`}
+    width = 0.45,          -- width of float window if not auto
+    height = 0.98,         -- height of float window if not auto
+    title_colors = 'nord', -- default to nord, one of {'nord', 'tokyo', 'dracula', 'rainbow', 'solarized ', 'monokai'}
+    -- can also set to a list of colors to define colors to choose from
+    -- e.g {'#D8DEE9', '#5E81AC', '#88C0D0', '#EBCB8B', '#A3BE8C', '#B48EAD'}
+  },
+  trouble = true,                                                              -- true: use trouble to open quickfix
+  test_efm = false,                                                            -- errorfomat for quickfix, default mix mode, set to true will be efm only
+  luasnip = false,                                                             -- enable included luasnip snippets. you can also disable while add lua/snips folder to luasnip load
+  --  Do not enable this if you already added the path, that will duplicate the entries
+  on_jobstart = function(cmd) _ = cmd end,                                     -- callback for stdout
+  on_stdout = function(err, data) _, _ = err, data end,                        -- callback when job started
+  on_stderr = function(err, data) _, _ = err, data end,                        -- callback for stderr
+  on_exit = function(code, signal, output) _, _, _ = code, signal, output end, -- callback for jobexit, output : string
+  iferr_vertical_shift = 4                                                     -- defines where the cursor will end up vertically from the begining of if err statement
 })
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
