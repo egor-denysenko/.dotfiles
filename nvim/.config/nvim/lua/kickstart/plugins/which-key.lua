b465efa639163c8ea670829ccffd8e83a4d14176
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2024-07-21 21:15:54 +0200

    chore(plugins): moved which key conf to new format

--- nvim/.config/nvim/lua/kickstart/plugins/which-key.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/which-key.lua
@@ -18,23 +18,25 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local wk = require 'which-key'
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[t]esting', _ = 'which_key_ignore' },
-        ['<leader>T'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      -- Document existing selection groups
+      wk.add {
+        { '<leader>s', desc = '', group = '[S]earch' },
+        { '<leader>c', desc = '', group = '[C]ode' },
+        { '<leader>d', desc = '', group = '[D]ocument' },
+        { '<leader>r', desc = '', group = '[R]ename' },
+        { '<leader>s', desc = '', group = '[S]earch' },
+        { '<leader>w', desc = '', group = '[W]orkspace' },
+        { '<leader>t', desc = '', group = '[t]esting' },
+        { '<leader>T', desc = '', group = '[T]oggle' },
+        { '<leader>h', desc = '', group = 'Git [H]unk' },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+
+      -- visual mode command selection
+      wk.add {
+        { '<leader>h', desc = '', group = 'Git [H]unk', mode = 'v' },
+      }
     end,
   },
 }
