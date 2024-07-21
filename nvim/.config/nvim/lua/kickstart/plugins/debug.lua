28daa4443ca83e4177dd7aabfd3dc5df2f6676bf
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2024-07-21 16:34:37 +0200

    chore(nvim): basic modular plugin setup

--- nvim/.config/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/debug.lua
@@ -19,8 +19,7 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    -- Custom debuggers can be found in custom/plugins
   },
   config = function()
     local dap = require 'dap'
@@ -81,14 +80,5 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
   end,
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -19,7 +19,7 @@ zstyle ':omz:update' frequency 7
 # DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 # You can also set it to another string to have that shown instead of the default red dots.
@@ -72,6 +72,8 @@ alias mux="zellij"
 alias air='/home/bronco/go/bin/air'
 alias pd='podman-compose'
 alias pdup='podman-compose up'
+
+alias update-goenv='cd $HOME/.goenv/plugins/go-build/../.. && git pull && cd $HOME'
 
 # Env variables export
 export SHELL="/bin/zsh"
