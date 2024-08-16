1dc8b8b2351f6a12844f96756dbe7b516b1ed049
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2024-08-16 23:58:42 +0200

    chore(generic): updated nvim deps and added go tools to script

--- scripts/scripts/go-tools.sh
+++ scripts/scripts/go-tools.sh
@@ -9,3 +9,6 @@ go install mvdan.cc/gofumpt@latest
 go install golang.org/x/tools/cmd/deadcode@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/segmentio/golines@latest
+go install github.com/goccy/go-graphviz@latest
+go install github.com/dkorunic/betteralign/cmd/betteralign@latest
+go install github.com/oligot/go-mod-upgrade@latest
