874d28143022836a6b72216ec0e6e6c1479585fa
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2026-06-12 12:32:54 +0200

    chore: remove temporary merge fix scripts

--- fix_merge_2.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('dot_config/scripts/executable_betterGitBranch.sh', 'r') as f:
-    content = f.read()
-
-content = content.replace('<<<<<<< HEAD\n\t    printf "${GREEN}%-${width1}s ${RED}%-${width2}s ${BLUE}%-${width3}s ${YELLOW}%-${width4}s ${NO_COLOR}%-${width5}s\\n" "$ahead" "$behind" "$branch" "$time" "$description"\n=======\n\t    printf "$row_format" "$ahead" "$behind" "$branch" "$time" "$description"\n>>>>>>> origin/chezmoi\n', '\t    printf "$row_format" "$ahead" "$behind" "$branch" "$time" "$description"\n')
-
-with open('dot_config/scripts/executable_betterGitBranch.sh', 'w') as f:
-    f.write(content)
